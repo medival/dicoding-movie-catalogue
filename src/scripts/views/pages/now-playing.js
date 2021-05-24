c7d606b5e06195efde2f71912d89710346c1d874
Adi Purnomo <adipurnomo811@gmail.com>
2021-05-24 22:57:53 +0700

    create: now-playing.js

---
+++ src/scripts/views/pages/now-playing.js
@@ -0,0 +1,17 @@
+/* eslint-disable semi */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-tabs */
+
+const NowPlaying = {
+  async render() {
+    return `
+		<h2> Now Playing Page </h2>
+	`;
+  },
+	
+  async afterRender() {
+    // Function after render
+  },
+}
+
+export default NowPlaying;
