e3608c106655914696bfd940155042b84ee95cf0
Adi Purnomo <adipurnomo811@gmail.com>
2021-05-24 23:02:51 +0700

    create: detail.js

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,17 @@
+/* eslint-disable semi */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-tabs */
+
+const Detail = {
+  async render() {
+    return `
+		<h2> Detail Page </h2>
+	`;
+  },
+
+  async afterRender() {
+    // Function After Render
+  },
+};
+
+export default Detail;
