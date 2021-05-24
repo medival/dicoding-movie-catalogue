510d2a861ec581e3800a53c6b1e6d2011d065072
Adi Purnomo <adipurnomo811@gmail.com>
2021-05-24 23:00:25 +0700

    create: upcoming.js

---
+++ src/scripts/views/pages/upcoming.js
@@ -0,0 +1,17 @@
+/* eslint-disable semi */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-tabs */
+
+const Upcoming = {
+  async render() {
+    return `
+		<h2> Upcoming page </h2>
+	`;
+  },
+	
+  async afterRender() {
+    // Function after render
+  },
+};
+
+export default Upcoming;
