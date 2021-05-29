e723d5fbd7d86c8979e98bd7fd17b8f8d5beec28
medival <dev.adipurnomo@gmail.com>
2021-05-29 12:23:46 +0700

    update: Import APIKEY to global-config

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,8 +1,10 @@
+import APIKEY from './api-key';
+
 const CONFIG = {
-	KEY: '',
-	BASE_URL: 'https://api.themoviedb.org/3/',
-	BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-	DEFAULT_LANGUAGE: 'en-us'
+  KEY: APIKEY,
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
 };
 
 export default CONFIG;
