79c88de64b959c03215b74442f6959075b4265ca
medival <dev.adipurnomo@gmail.com>
2021-05-29 12:45:36 +0700

    update: Import APIKEY with backtick

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,10 +1,12 @@
 import APIKEY from './api-key';
 
 const CONFIG = {
-  KEY: APIKEY,
+  KEY: `${APIKEY}`,
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
 };
+
+console.log(`${APIKEY}`);
 
 export default CONFIG;
