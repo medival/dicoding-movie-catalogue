469d7602afe34100c0a718b448780b0c7e9c19d9
medival <dev.adipurnomo@gmail.com>
2021-05-31 01:51:06 +0700

    update: Create CRUD function

--- src/scripts/data/favoritemovie-idb.js
+++ src/scripts/data/favoritemovie-idb.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import { ids } from 'webpack';
 import CONFIG from '../global/config';
 
 const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORE_NAME } = CONFIG;
@@ -8,4 +9,21 @@ const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
     database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
   },
 });
+
+const FavoriteMovieIdb = {
+  async getMovie(id) {
+    return (await dbPromise).get(OBJECT_STORE_NAME, id);
+  },
+  async getAllMovies() {
+    return (await dbPromise).getAll(OBJECT_STORE_NAME);
+  },
+  async putMovies(movie) {
+    return (await dbPromise).put(OBJECT_STORE_NAME, movie);
+  },
+  async deleteMovie(id) {
+    return (await dbPromise).delete(OBJECT_STORE_NAME, id);
+  },
+};
+
+export default FavoriteMovieIdb;
 
